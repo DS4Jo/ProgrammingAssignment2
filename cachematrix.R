aa7eac9a8310a2deda17eae474ea23581ce153cd
DS4Jo <job4jo@yahoo.comgit initgit remote add origin https://github.com/DS4Jo/git config --global user.email job4jo@yahoo.com>
2015-10-24 17:38:01 -0700

    submit completed cacheMatrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix functions
 
-## Write a short comment describing this function
+## 2015/10/24 Assignment2 Coursera R Programming
+## These functions are intended to compute the inverse of
+## a matrix and caches the results so that an unchanged
+## matrix need not be reevaluated.
+## NOTE that this assumes the matrix passed as the
+## argument is a square matrix and is always invertible.
+
+
+## makeCacheMatrix creates a special "matrix" object
+## that will cache the inverse of the matrix x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m<-NULL
+     set<-function(y){
+          x<<-y
+          m<<-NULL
+     }
+     get<-function() x
+     setmatrixx<-function(solve) m<<- solve
+     getmatrixx<-function() m
+     list(set=set, get=get,
+          setmatrixx=setmatrixx,
+          getmatrixx=getmatrixx)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix"
+## object returned by makeCacheMatrix (above). If the object x is in
+## cache and unchanged, the cached object will be used however if
+## it does not exist, will proceed to compute the inverse of 
+## the square matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     m<-x$getmatrixx()
+     if(!is.null(m)){
+          message("getting cached data")
+          return(m)
+     }
+     datam <-x$get()
+     m<-solve(datam, ...)
+     x$setmatrixx(m)
+     m
 }
